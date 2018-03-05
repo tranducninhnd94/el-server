f69941f4ec0464cdfdd88874356c46ee4d1815f8
Tran Duc Ninh <tranducninhnd94@hotmail.com>
2018-03-06 01:25:24 +0700

    statement

--- common/constants.js
+++ common/constants.js
@@ -13,4 +13,69 @@ module.exports = {
     CLIENT_SEND_MESSAGE: "Client_send_message",
     CLIENT_SEND_INFO: "Client_send_info",
 
+    // db.getCollection('posts').aggregate(
+    //     [
+    //         {
+    //             $match: {
+    //                 status : "SHOW"
+    //             }
+    //         },
+    //         {
+    //             $lookup:{
+    //                 from: "users",
+    //                 localField: "user",
+    //                 foreignField: "_id",
+    //                 as: "creator"
+    //             }
+    //         },
+    //         {
+    //             $lookup: {
+    //                 from : "comments",
+    //                 localField: "_id",
+    //                 foreignField: "post",
+    //                 as: "comments"
+    //             }
+    //         },
+    //         {
+    //             $unwind: {
+    //                 path: "$comments",
+    //                 preserveNullAndEmptyArrays: true
+    //             }
+    //         },
+           
+            
+    //         {
+    //             $project:{
+    //                 "_id": 1,
+    //                 "file_upload": 1,
+    //                 "status": 1,
+    //                 "create_at": 1,
+    //                 "title": 1,
+    //                 "content": 1,
+    //                 "description": 1,
+    //                 "user": "$creator",
+    //                 "total_replies": { $ifNull: ["$comments.total_replies"  , 0]},
+    //                 "comments" : {$cond : [{$not: ["$comments"]}, 0, 1 ] }
+    //                 }
+    //         },
+           
+    //          {
+    //             $group:{
+    //                 _id : "$_id",
+    //                 file_upload : { $first: '$file_upload' },
+    //                 status : { $first: '$status' },
+    //                 create_at : { $first: '$create_at' },
+    //                 title : { $first: '$title' },
+    //                  description : { $first: '$description' },
+    //                 content : { $first: '$content' },
+    //                 user : { $first: '$user' },
+    //                 total_comment: {$sum :{ $sum: [ "$comments", "$total_replies" ] }}
+    //             }
+    //         }
+        
+    //     ]
+    
+    
+    // )
+
 }
